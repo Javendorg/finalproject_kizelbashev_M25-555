37a772eac38cd67d6007a711e1cf0f70f7c14bf7
javendorg <javendorg777@yandex.ru>
2026-01-07 17:14:11 +0300

    final_commit

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print('hello world')
+#!/usr/bin/env python3
+from valutatrade_hub.cli.interface import InteractiveCLI
+
+
+def main():  
+    cli = InteractiveCLI()
+    cli.run()
+
+if __name__ == "__main__":
+    main()
